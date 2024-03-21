382ce5cc99911582544819300980fcd5c72832fd
Guegouo M. Guiddel <guegouomoghommahieguiddel@gmail.com>
2024-03-21 03:53:16 +0100

    First commit

---
+++ src/main/java/edu/tcu/cs/demo/HogwartArtifactOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartArtifactOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartArtifactOnlineApplication.class, args);
+	}
+
+}
